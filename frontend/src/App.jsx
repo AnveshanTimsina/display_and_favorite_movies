29b358b3aa1bf51f001b11108343aeba42515fee
Anveshan Timsina <timsinaanveshan@gmail.com>
2025-12-30 22:15:13 +0545

    add Home, Favorites, and MovieCard components with search functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css'
-import MovieCard from './MovieCard.jsx'
+import MovieCard from './components/MovieCard.jsx'
+import Home from './pages/Home.jsx'
+import Favorites from './pages/Favorites.jsx'
 
 function App() {
 
   return (
     <>
-      <MovieCard movie={{title: "Test Movie1", release_date: "2020"}} />
-      <MovieCard movie={{title: "Test Movie2", release_date: "2021"}} />
-      <MovieCard movie={{title: "Test Movie3", release_date: "2022"}} />
-      <MovieCard movie={{title: "Test Movie4", release_date: "2023"}} />
+      <Home/>
     </>
   )
 }
